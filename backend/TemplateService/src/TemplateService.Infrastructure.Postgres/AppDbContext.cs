bbe8c05fb2cad4ff1878a58e8780bdce6466be98
Damir <damirzasimenko@gmail.com>
2026-07-09 19:17:19 +0300

    Update appdbcontext

--- backend/TemplateService/src/TemplateService.Infrastructure.Postgres/AppDbContext.cs
+++ backend/TemplateService/src/TemplateService.Infrastructure.Postgres/AppDbContext.cs
@@ -5,8 +5,22 @@ namespace TemplateService.Infrastructure.Postgres;
 public sealed class AppDbContext : DbContext
 {
     public AppDbContext(
-        DbContextOptions<AppDbContext> options)
+        DbContextOptions<AppDbContext> options, string connectionString)
         : base(options)
     {
+        
+        _connectionString = connectionString;
+    }
+
+    private readonly string _connectionString;
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseNpgsql(_connectionString);
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        
     }
 }
